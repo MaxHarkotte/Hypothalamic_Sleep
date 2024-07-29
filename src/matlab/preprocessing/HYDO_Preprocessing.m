7201b70ca27a5a1b6b6798d63e43421a3ba8c2d1
MaxHarkotte <maximilian.harkotte@gmail.de>
2024-07-29 14:08:00 +0200

    Update Preprocessing Pipeline
    add laplacian re-referencing scheme
    add .edf output
    update comments

--- src/matlab/preprocessing/HYDO_Preprocessing.m
+++ src/matlab/preprocessing/HYDO_Preprocessing.m
@@ -23,15 +23,42 @@ reference = readtable(strcat(file_server_path, 'Hypothalamic_Sleep/data/raw/refe
 %% Select recordings for preprocessing 
 selection             = {'2024-05-21_10-28-00', '2024-07-22_10-47-00'};    
 
-%% Preprocessing
-% Prepare .edf file to write data to 
-cfg            = [];
-cfg.dataset    = strcat(strcat(file_server_path, 'Hypothalamic_Sleep/data/edfexample/Osas2002.edf')); % change dir to local if needed
-cfg.continuous = 'yes';
-cfg.channel    = 'EEG Fpz-M2';
-ft_dummy_dat   = ft_preprocessing(cfg); 
+%% Channel map for A1x32-Poly3-10mm-50-177
+channel_neighbors = containers.Map;
+channel_neighbors('Probe_2') = {'Probe_1', 'Probe_16'};
+channel_neighbors('Probe_1') = {'Probe_2', 'Probe_3', 'Probe_18'};
+channel_neighbors('Probe_3') = {'Probe_1', 'Probe_4', 'Probe_15'};
+channel_neighbors('Probe_4') = {'Probe_3', 'Probe_5', 'Probe_19'};
+channel_neighbors('Probe_5') = {'Probe_4', 'Probe_6', 'Probe_14'};
+channel_neighbors('Probe_6') = {'Probe_5', 'Probe_7', 'Probe_20'};
+channel_neighbors('Probe_7') = {'Probe_6', 'Probe_8', 'Probe_13'};
+channel_neighbors('Probe_8') = {'Probe_7', 'Probe_9', 'Probe_21'};
+channel_neighbors('Probe_9') = {'Probe_8', 'Probe_10', 'Probe_12'};
+channel_neighbors('Probe_10') = {'Probe_9', 'Probe_22'};
+channel_neighbors('Probe_17') = {'Probe_16'};
+channel_neighbors('Probe_16') = {'Probe_2', 'Probe_17', 'Probe_18', 'Probe_31'};
+channel_neighbors('Probe_18') = {'Probe_1', 'Probe_15', 'Probe_16', 'Probe_32'};
+channel_neighbors('Probe_15') = {'Probe_3', 'Probe_18', 'Probe_19', 'Probe_30'};
+channel_neighbors('Probe_19') = {'Probe_4', 'Probe_14', 'Probe_15', 'Probe_29'};
+channel_neighbors('Probe_14') = {'Probe_5', 'Probe_19', 'Probe_20', 'Probe_28'};
+channel_neighbors('Probe_20') = {'Probe_6', 'Probe_13', 'Probe_14', 'Probe_27'};
+channel_neighbors('Probe_13') = {'Probe_7', 'Probe_20', 'Probe_21', 'Probe_26'};
+channel_neighbors('Probe_21') = {'Probe_8', 'Probe_12', 'Probe_13', 'Probe_25'};
+channel_neighbors('Probe_12') = {'Probe_9', 'Probe_21', 'Probe_22', 'Probe_24'};
+channel_neighbors('Probe_22') = {'Probe_10', 'Probe_11', 'Probe_12', 'Probe_23'};
+channel_neighbors('Probe_11') = {'Probe_22'};
+channel_neighbors('Probe_31') = {'Probe_16', 'Probe_32'};
+channel_neighbors('Probe_32') = {'Probe_18', 'Probe_30', 'Probe_31'};
+channel_neighbors('Probe_30') = {'Probe_15', 'Probe_29', 'Probe_32'};
+channel_neighbors('Probe_29') = {'Probe_19', 'Probe_28', 'Probe_30'};
+channel_neighbors('Probe_28') = {'Probe_14', 'Probe_27', 'Probe_29'};
+channel_neighbors('Probe_27') = {'Probe_20', 'Probe_26', 'Probe_28'};
+channel_neighbors('Probe_26') = {'Probe_13', 'Probe_25', 'Probe_27'};
+channel_neighbors('Probe_25') = {'Probe_21', 'Probe_24', 'Probe_26'};
+channel_neighbors('Probe_24') = {'Probe_12', 'Probe_23', 'Probe_25'};
+channel_neighbors('Probe_23') = {'Probe_22', 'Probe_24'};
 
-% preprocess recordings 
+%% Preprocessing
 for iRec = 1:size(selection,2)
 
     % Recording information
@@ -88,22 +115,104 @@ for iRec = 1:size(selection,2)
 
     clear datr; 
 
-    % Re-reference channels of Probe 
+    % Re-reference channels of Probe to average of all elecs
     cfg = [];
     cfg.channel = 'all'; 
     cfg.reref = 'yes';
     cfg.refmethod = 'avg';
     cfg.refchannel = 'all';
-    LFP_reref = ft_preprocessing(cfg, LFP_downsample);
+    LFP_avg = ft_preprocessing(cfg, LFP_downsample);
 
-    % Write data as .edf file 
-    cfg       = [];
-    rec_clean = ft_appenddata(cfg, EEGEMG_downsample, LFP_reref);
+    % Re-reference channels of Probe locally using bipolar/laplace scheme 
+    LFP_bipolar = LFP_downsample;
+    exclude_channels = {};
+    iExcl = 1;
 
-    % Change header information
+    for iChan = 1:size(Probe_chans, 1)
+        % list possible reference channels
+        neighbors = channel_neighbors(char(Probe_chans(iChan)));
+        [isPresent, idx] = ismember(neighbors, LFP_downsample.label);
+       
+        % Re-reference with the average of all existing channels that are
+        % 50 microns away from the elec
+        if isPresent
+            LFP_bipolar.trial{1,1}(iChan,:) = LFP_downsample.trial{1,1}(iChan, :) - mean(LFP_downsample.trial{1,1}(idx(isPresent),:),1);
+        else
+            exclude_channels(iExcl) = Probe_chans(iChan);
+            iExcl = iExcl +1;
+        end
+    end
+
+    % Exclude channels for bipolar re-referencing, if no neighoring channel
+    % was available
+    if ~isempty(exclude_channels)
+        exclude_channels = strcat('-', exclude_channels);
+        cfg = [];
+        cfg.channel = [{'all'}, exclude_channels]; 
+        LFP_bipolar = ft_selectdata(cfg, LFP_bipolar);
+    end
+
+    % Create general header info 
+    hdr.Fs = 250; 
+    hdr.nSamples = size(EEGEMG_downsample.trial{1,1},2);
+    hdr.SamplesPre = 0;
+    hdr.nTrials = 1;
+
+    % create channel specific header info 
+    EEGEMG_downsample.hdr          = hdr; 
+    EEGEMG_downsample.hdr.label    = EEGEMG_downsample.label; 
+    EEGEMG_downsample.hdr.nCans    = size(EEGEMG_downsample.label,1);
+    EEGEMG_downsample.hdr.chantype = EEGEMG_downsample.label;
+    units                          = cell(size(EEGEMG_downsample.label,1), 1);
+    units(:)                       = {'uV'};
+    EEGEMG_downsample.hdr.chanunit = units;
+    hdr_EEGEMG                     = ft_fetch_header(EEGEMG_downsample);
+    clear units; 
+
+    LFP_avg.hdr          = hdr; 
+    LFP_avg.hdr.label    = LFP_avg.label; 
+    LFP_avg.hdr.nCans    = size(LFP_avg.label,1);
+    LFP_avg.hdr.chantype = LFP_avg.label;
+    units                = cell(size(LFP_avg.label,1), 1);
+    units(:)             = {'uV'};
+    LFP_avg.hdr.chanunit = units;
+    hdr_LFP_avg          = ft_fetch_header(LFP_avg);
+    clear units;
+
+    LFP_bipolar.hdr          = hdr; 
+    LFP_bipolar.hdr.label    = LFP_bipolar.label; 
+    LFP_bipolar.hdr.nCans    = size(LFP_bipolar.label,1);
+    LFP_bipolar.hdr.chantype = LFP_bipolar.label;
+    units                    = cell(size(LFP_bipolar.label,1), 1);
+    units(:)                 = {'uV'};
+    LFP_bipolar.hdr.chanunit = units;
+    hdr_LFP_bipolar          = ft_fetch_header(LFP_bipolar);
+    clear units; 
+
+    % create directory to store downsampled data
+    if ~exist(char(strcat(file_server_path, 'Hypothalamic_Sleep/data/processed/', rec_info.id, '/', rec_info.file, '/')), 'dir')
+        mkdir(char(strcat(file_server_path, 'Hypothalamic_Sleep/data/processed/', rec_info.id, '/', rec_info.file, '/')))
+    end
     
+    % create directory to store sleep scorings (done manually later)
+    if ~exist(char(strcat(file_server_path, 'Hypothalamic_Sleep/data/scoring/', rec_info.id, '/', rec_info.file, '/')), 'dir')
+        mkdir(char(strcat(file_server_path, 'Hypothalamic_Sleep/data/scoring/', rec_info.id, '/', rec_info.file, '/')))
+    end
+    
+    % Write .edf files 
+    cd(char(strcat(file_server_path, 'Hypothalamic_Sleep/data/processed/', rec_info.id, '/', rec_info.file, '/')))
+    
+    % EEG and EMG channels
+    ft_write_data(strcat(char(rec_info.file), '_EEGEMG_250Hz.edf') ...
+    , EEGEMG_downsample.trial{1}, 'header', hdr_EEGEMG,'chaninx', EEGEMG_downsample.label', 'dataformat', "edf")
+    
+    % LFP channels rereferenced to average of all probe elecs
+    ft_write_data(strcat(char(rec_info.file), '_LFP_avg_250Hz.edf') ...
+    , LFP_avg.trial{1}, 'header', hdr_LFP_avg,'chaninx', LFP_avg.label', 'dataformat', "edf")
 
-
+    % LFP channels rereferenced with laplace/biploar method
+    ft_write_data(strcat(char(rec_info.file), '_LFP_bipolar_250Hz.edf') ...
+    , LFP_bipolar.trial{1}, 'header', hdr_LFP_bipolar,'chaninx', LFP_bipolar.label', 'dataformat', "edf")
 
 end
 
