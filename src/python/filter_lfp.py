ac4827cfdd6f3d4b9a7e32d2addb75dda50b2b1f
dpeg22 <dpeg22@gmail.com>
2025-01-27 14:38:35 +0100

    begin event detection implementation in python

---
+++ src/python/filter_lfp.py
@@ -0,0 +1,123 @@
+## filter_lfp.py
+
+import numpy as np
+import psutil
+from scipy import signal
+import spikeinterface as si
+import spikeinterface.extractors as se
+import ghostipy as gsp
+from pathlib import Path, PosixPath
+from typing import List
+
+
+def load_rec(recording_path, concatenate=True, channels=None):
+    recording = se.read_neuralynx(recording_path)
+    if recording.get_num_segments() > 1:
+        if concatenate:
+            timestamps = []
+            for seg in recording._recording_segments:
+                timestamps.extend(seg.get_times())
+            concat_recording = si.ConcatenateSegmentRecording([recording])
+            concat_recording.set_times(np.array(timestamps))
+            recording = concat_recording
+    if channels:
+        if channels == "all":
+            recording = recording
+        elif isinstance(channels, List):
+            if any(channel not in recording.get_channel_ids() for channel in channels):
+                raise ValueError(f">= 1 channel of {channels} not found in recording.")
+            recording = recording.channel_slice(channel_ids=channels)
+    return recording
+
+
+def get_recording_path(base_path, animal, date, session):
+    if not isinstance(base_path, PosixPath):
+        base_path = Path(base_path)
+    data_path = Path(base_path, animal, date)
+    if not data_path.exists():
+        raise ValueError(f"Data path {data_path} does not exist.")
+    return data_path
+
+
+def get_valid_times(recording, atol=1e-6):
+    timestamps = recording.get_times()
+    dt = 1 / recording.get_time_info()["sampling_frequency"]
+    time_diff = np.diff(timestamps)
+    jump_times = np.concatenate(([0], np.where(time_diff - dt > atol)[0]))
+    valid_times = [
+        (timestamps[i], timestamps[jump_times[i + 1]])
+        for i in range(len(jump_times) - 1)
+    ]
+    return valid_times
+
+
+def time_bound_check(start, stop, timestamps, n_samples):
+    if start < timestamps[0]:
+        start = timestamps[0]
+    if stop > timestamps[-1]:
+        stop = timestamps[-1]
+    frm, to = np.searchsorted(timestamps, (start, stop))
+    to = min(to, n_samples)
+    return frm, to
+
+
+def filter_data(recording, filter_coeff, valid_times, decimation=None, channels=None):
+    import dask
+
+    if channels is None:
+        channels = recording.get_channel_ids()
+    timestamps = dask.delayed(recording.get_times())
+    n_samples = recording.get_num_samples()
+    ram_capacity = psutil.virtual_memory().available / (1024**3) * 0.9
+    rec_disk_mem = recording.get_memory_size() / (1024**3)
+    data_on_disk = dask.delayed(recording.get_traces(channel_ids=channels))
+
+    indices = []
+    output_shape_list = [0] * 2
+    output_shape_list[1] = len(channels)
+    output_offsets = [0]
+    filter_delay = (len(filter_coeff) - 1) // 2
+    if rec_disk_mem > ram_capacity:
+        print("Recording size exceeds available RAM. Loading recording in chunks.")
+        for start, stop in valid_times:
+            frm, to = time_bound_check(start, stop, timestamps, n_samples)
+
+            indices.append((frm, to))
+            shape, _ = gsp.filter_data_fir(
+                data_on_disk,
+                filter_coeff,
+                axis=0,
+                input_index_bounds=[frm, to - 1],
+                output_index_bounds=[filter_delay, filter_delay + to - frm],
+                describe_dims=True,
+                ds=decimation,
+                input_dim_restrictions=[None, np.s_[channels]],
+            )
+            output_offsets.append(output_offsets[-1] + shape[0])
+            output_shape_list[0] += shape[0]
+        filtered_data = np.empty(
+            tuple(output_shape_list), dtype=data_on_disk.dtype.compute()
+        )
+        results = []
+        new_timestamps = np.empty((output_shape_list[0],), timestamps.dtype.compute())
+        indices = np.array(indices, ndmin=2)
+        ts_offset = 0
+        for i, (start, end) in enumerate(valid_times):
+            extracted_ts = timestamps[start:stop:decimation]
+            new_timestamps[ts_offset : ts_offset + len(extracted_ts)] = extracted_ts
+            ts_offset += len(extracted_ts)
+            results.append(
+                dask.delayed(gsp.filter_data_fir)(
+                    data_on_disk,
+                    filter_coeff,
+                    axis=0,
+                    input_index_bounds=[start, stop],
+                    output_index_bounds=[filter_delay, filter_delay + to - frm],
+                    outarray=filtered_data,
+                    ds=decimation,
+                    input_dim_restrictions=[None, np.s_[channels]],
+                    output_offset=output_offsets[i],
+                )
+            )
+    filtered_data_tmp = dask.compute(*results)
+    return filtered_data, new_timestamps
